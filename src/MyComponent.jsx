b63e044c74dce7545d5951b10441351eb6941616
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-10 18:08:45 +0600

    addFood

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,19 +5,25 @@ function MyComponent(){
   const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
 
   function handleAddFood(){
+      const newFood = document.getElementById("foodInput").value;
+      document.getElementById("foodInput").value = "";
 
+      setFoods([newFood]);
   }
 
   function handleRemoveFood(){
 
   }
 
+
+
   return( <div>
             <h2>List of Food</h2>
             <ul>
               {foods.map((food, index) => <li key={index}>{food}</li>)}
             </ul>
-
+            <input type="text" id="foodInput" placeholder="Enter Food Items"/>
+            <button onClick={handleAddFood}>Add Food</button>
           </div>);
 }
 export default MyComponent
